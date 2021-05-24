64fecb182e66f92426e8cb7c3020a864aa1e4bc3
VandanaGhori5697 <vandanaghori19@gmail.com>
2021-05-24 11:01:10 -0400

    all files added.

---
+++ app/src/main/java/com/example/moviessearchapi/MainActivity.java
@@ -0,0 +1,14 @@
+echo "# movies-search-api-demo" >> README.mdpackage com.example.moviessearchapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
